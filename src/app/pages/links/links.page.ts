482b463e350c8bb4280e20732044816b8ef6bc80
Nicolas B. <46492476+nicolegrimpeur@users.noreply.github.com>
2025-10-05 16:44:57 +0200

    Clean du header + gestion téléphone

--- src/app/pages/links/links.page.ts
+++ src/app/pages/links/links.page.ts
@@ -1,14 +1,22 @@
 import {Component, OnInit} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {FormsModule} from '@angular/forms';
-import {IonContent, IonHeader, IonTitle, IonToolbar} from '@ionic/angular/standalone';
+import {
+  IonCard,
+  IonCardContent,
+  IonCardHeader, IonCardTitle,
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-links',
   templateUrl: './links.page.html',
   styleUrls: ['./links.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, CommonModule, FormsModule, IonCard, IonCardContent, IonCardHeader, IonCardTitle]
 })
 export class LinksPage implements OnInit {
 
